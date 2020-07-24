f078a9834d0f42af9d70c26c0559e655f140d602
B. Endres <endres@systopia.de>
2020-07-24 11:31:37 -0500

    [#2] implemented email report

---
+++ api/v3/DBMonitor.php
@@ -0,0 +1,91 @@
+<?php
+/*-------------------------------------------------------+
+| DB Monitoring                                          |
+| Copyright (C) 2020 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Dbmonitor_ExtensionUtil as E;
+
+/**
+ * Adjust Metadata for DBMonitor.probe
+ */
+function _civicrm_api3_d_b_monitor_probe_spec(&$params)
+{
+    $params['email_recipients'] = array(
+        'name'         => 'email_recipients',
+        'api.required' => 0,
+        'title'        => 'Email recipients',
+        'description'  => 'Comma-separated list of recipients. If empty uses current user',
+    );
+}
+
+
+/**
+ * API Action DBMonitor.probe
+ *
+ * Check the system for stuck queries and send an email if there are any
+ */
+function civicrm_api3_d_b_monitor_probe(&$params)
+{
+    $queries = CRM_Dbmonitor_Monitor::getStuckQueries();
+    if (empty($queries)) {
+        // no stuck queries detected
+        return civicrm_api3_create_success(E::ts("No stuck queries detected"));
+    }
+
+    // find out recipient emails
+    $recipients_emails = [];
+    if (empty($params['email_recipients'])) {
+        $contact_id = CRM_Core_Session::getLoggedInContactID();
+        if ($contact_id) {
+            try {
+                $recipients_emails[] = civicrm_api3(
+                    'Email',
+                    'getvalue',
+                    [
+                        'return'       => 'email',
+                        'contact_id'   => $contact_id,
+                        'is_primary'   => 1,
+                        'option.limit' => 1,
+                    ]
+                );
+            } catch (CiviCRM_API3_Exception $ex) {
+                // contact doesn't seem to have a primary email
+            }
+        }
+
+    } else {
+        foreach (preg_split('/,/', $params['email_recipients']) as $email) {
+            $email = trim($email);
+            if ($email) {
+                $recipients_emails[] = $email;
+            }
+        }
+    }
+
+    if (empty($recipients_emails)) {
+        if (empty($params['email_recipients'])) {
+            return civicrm_api3_create_error(E::ts("Current user has no valid email."));
+        } else {
+            return civicrm_api3_create_error(E::ts("No valid email addresses provided."));
+        }
+    }
+
+    // all good: render and send
+    CRM_Dbmonitor_Monitor::sendEmailReport($recipients_emails, $queries);
+
+    return civicrm_api3_create_success(E::ts("%1 stuck queries sent to %2 recipient(s)", [
+        1 => count($queries),
+        2 => count($recipients_emails)
+    ]));
+}
+
+
---
+++ templates/probe_email.tpl
@@ -0,0 +1,39 @@
+{*-------------------------------------------------------+
+| DB Monitoring                                          |
+| Copyright (C) 2020 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++-------------------------------------------------------*}
+
+{crmScope extensionKey='de.systopia.dbmonitor'}
+ <body>
+    <h3>{ts}Conspicuous queries in CiviCRM's database{/ts}</h3>
+    <table>
+        <thead>
+        <tr>
+            <th>{ts}Process ID{/ts}</th>
+            <th>{ts}Status{/ts}</th>
+            <th>{ts}Query{/ts}</th>
+            <th>{ts}Running Since{/ts}</th>
+        </tr>
+        </thead>
+        <tbody>
+        {foreach from=$queries item=query}
+            {assign var="query_id" value=$query.id}
+            <tr>
+                <td>{$query.id}</td>
+                <td>{$query.state}</td>
+                <td><a title="{$query.sql}">{$query.sql_short}</a></td>
+                <td>{$query.runtime_text}</td>
+            </tr>
+        {/foreach}
+        </tbody>
+    </table>
+ </body>
+{/crmScope}
