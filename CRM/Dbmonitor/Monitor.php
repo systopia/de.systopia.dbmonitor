a15b4647cf2832d0da4c6579b7c09a6239864bc9
Benni Baermann <baermann@systopia.de>
2026-08-06 10:59:11 +0200

    all phpstan level 5 errors fixed or explizitly ignored

--- CRM/Dbmonitor/Monitor.php
+++ CRM/Dbmonitor/Monitor.php
@@ -238,7 +238,7 @@ class CRM_Dbmonitor_Monitor {
       // render content
       $smarty = CRM_Core_Smarty::singleton();
       $smarty->assign('queries', $queries);
-      $smarty->assign('dbmonitorlink', CRM_Utils_System::url('civicrm/admin/dbprocesslist', NULL, TRUE));
+      $smarty->assign('dbmonitorlink', CRM_Utils_System::url('civicrm/admin/dbprocesslist', '', TRUE));
       $smarty_template = E::path('templates/probe_email.tpl');
       $email['html'] = $smarty->fetch($smarty_template);
 
